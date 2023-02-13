902e8c3538ac4030b6c227a865e53179faed5461
shivam456 <shivam14098@iiitd.ac.in>
2023-02-13 02:23:10 -0500

    hop

--- src/components/vitals/VitalAdd.js
+++ src/components/vitals/VitalAdd.js
@@ -9,12 +9,13 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import fhirdata from 'src/__mocks__/fhirdata';
 import { FhirClientContext } from 'src/FhirClientContext';
+import { client } from 'fhirclient';
+import Client from 'fhirclient/lib/Client';
 
 export default class VitalAdd extends React.Component {
     static contextType = FhirClientContext;
 
     constructor(props) {
-
       super(props);
       this.state = {
         loading: true,
@@ -29,68 +30,66 @@ export default class VitalAdd extends React.Component {
 
     componentDidMount() {
       this.setState({ client: this.context.client });
-      console.log(this.context.client);
     }
 
     render() {
       const handleClose = () => {
-        const vitaltypes = ['rr'];
         this.setState({ open: false });
+      };
+
+      const handleSubmit = () => {
+        const vitaltypes = ['rr'];
+        this.setState({ open: true });
 
 /* START TO EDIT YOUR CODE HERE: Task 3 update your code here. You will have to use the SMART API to add the ability to add the  
  Respiratory rate (rr) observation with the user input */ 
+        const today = new Date();
 
- const today = new Date();
-
- var data = {
-   "resourceType": "Observation",
-   "id": "1463412",
-   "meta": {
-     "versionId": "1",
-     "lastUpdated": today.toISOString()
-   },
-   "status": "final",
-   "category": [
-     {
-       "coding": [
-         {
-           "system": "http://terminology.hl7.org/CodeSystem/observation-category",
-           "code": "vital-signs",
-           "display": "vital-signs"
-         }
-       ]
-     }
-   ],
-   "code": {
-     "coding": [
-       {
-         "system": "http://loinc.org",
-         "code": "9279-1",
-         "display": "Respiratory rate"
-       }
-     ],
-     "text": "Respiratory rate"
-   },
-   "subject": {
-     "reference": "Patient/"+this.context.client.patient.id
-   },
-   "effectiveDateTime": today.toISOString(),
-   "issued": today.toISOString(),
-   "valueQuantity": {
-     "value": rr.value,
-     "unit": "breaths/min",
-     "system": "http://unitsofmeasure.org",
-     "code": "/min"
-   }
- }
- this.context.client.create(data);
-        
+        var data = {
+          "resourceType": "Observation",
+          "id": "1463412",
+          "meta": {
+            "versionId": "1",
+            "lastUpdated": today.toISOString()
+          },
+          "status": "final",
+          "category": [
+            {
+              "coding": [
+                {
+                  "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+                  "code": "vital-signs",
+                  "display": "vital-signs"
+                }
+              ]
+            }
+          ],
+          "code": {
+            "coding": [
+              {
+                "system": "http://loinc.org",
+                "code": "9279-1",
+                "display": "Respiratory rate"
+              }
+            ],
+            "text": "Respiratory rate"
+          },
+          "subject": {
+            "reference": "Patient/"+this.context.client.patient.id
+          },
+          "effectiveDateTime": today.toISOString(),
+          "issued": today.toISOString(),
+          "valueQuantity": {
+            "value": rr.value,
+            "unit": "breaths/min",
+            "system": "http://unitsofmeasure.org",
+            "code": "/min"
+          }
+        }
+        this.context.client.create(data);
 
  /* END OF WHERE YOU NEED TO EDIT YOUR CODE */ 
  
-        this.setState({
-          rr: '',
-        });
         this.props.getVitals();
       };
 
@@ -125,7 +124,7 @@ export default class VitalAdd extends React.Component {
               <Button onClick={handleClose} color="primary">
                 Cancel
               </Button>
-              <Button onClick={handleClose} color="primary">
+              <Button onClick={handleSubmit} color="primary">
                 Sumbit Vitals
               </Button>
             </DialogActions>
