6e0e2e0686af9ced770ae64699bd029d6b23a5d8
shivam456 <shivam14098@iiitd.ac.in>
2023-02-13 04:06:58 -0500

    hoe

--- src/Launcher.js
+++ src/Launcher.js
@@ -18,22 +18,22 @@ export default class Launcher extends React.Component {
    * is loaded directly. An EHR can still launch it by passing `iss`
    * and `launch` url parameters
    */
-  componentDidMount() {
-    SMART.authorize({
-      clientId: '0e0fb8fd-dfe2-4f45-9b10-f33533297702',
-      scope: 'launch launch/patient patient/read offline_access patient/Patient.read patient/Observation.read patient/Observation.write patient/MedicationRequest.read ',
-      redirectUri: 'https://newapp1234.herokuapp.com/app/dashboard',
-      //iss: 'https://fhir-ehr-code.cerner.com/r4/',
-      iss: 'https://r4.smarthealthit.org',
+  // componentDidMount() {
+  //   SMART.authorize({
+  //     clientId: '0e0fb8fd-dfe2-4f45-9b10-f33533297702',
+  //     scope: 'launch launch/patient patient/read offline_access patient/Patient.read patient/Observation.read patient/Observation.write patient/MedicationRequest.read ',
+  //     redirectUri: 'https://newapp1234.herokuapp.com/app/dashboard',
+  //     //iss: 'https://fhir-ehr-code.cerner.com/r4/',
+  //     iss: 'https://r4.smarthealthit.org',
 
-      completeInTarget: false
-    });
+  //     completeInTarget: false
+  //   });
 
-    FHIR.oauth2.ready()
-    .then(client => client.request("Patient"))
-    .then(console.log('test'))
-    .catch(console.error);
-  }
+  //   FHIR.oauth2.ready()
+  //   .then(client => client.request("Patient"))
+  //   .then(console.log('test'))
+  //   .catch(console.error);
+  // }
   
 
 
@@ -43,21 +43,21 @@ export default class Launcher extends React.Component {
 
 
 
-// componentDidMount() {
-//   SMART.authorize({
-//     clientId: 'my-client-id',
-//     scope: 'launch launch/patient patient/read offline_access',
-//     redirectUri: './app/dashboard',
-//     iss: 'https://launch.smarthealthit.org/v/r4/sim/eyJoIjoiMSIsImIiOiI2N2NiZjA5MC00ZGRiLTQ3OTktOTlmZi1hMjhhYmUyNzQwYjEiLCJqIjoiMSJ9/fhir',
+componentDidMount() {
+  SMART.authorize({
+    clientId: '0e0fb8fd-dfe2-4f45-9b10-f33533297702',
+    scope: 'launch launch/patient patient/read offline_access',
+    redirectUri: 'https://newapp1234.herokuapp.com/app/dashboard',
+    iss: 'https://launch.smarthealthit.org/v/r4/sim/eyJoIjoiMSIsImIiOiI2N2NiZjA5MC00ZGRiLTQ3OTktOTlmZi1hMjhhYmUyNzQwYjEiLCJqIjoiMSJ9/fhir',
 
-//     completeInTarget: false
-//   });
+    completeInTarget: false
+  });
 
-//   FHIR.oauth2.ready()
-//   .then(client => client.request("Patient"))
-//   .then(console.log('test'))
-//   .catch(console.error);
-// }
+  FHIR.oauth2.ready()
+  .then(client => client.request("Patient"))
+  .then(console.log('test'))
+  .catch(console.error);
+}
 
 render() {
   return 'Launching...';
